ed11282831b7f9b78621eb75d818abab465b33f0
saifuddien <ariefsaifuddien01@gmail.com>
2022-06-03 01:20:47 +0200

    finish

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,35 +1,341 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import quraa from './quraa.json'
 
 function InputField() {
 
-  const get = async () => {
-    return await fetch('http://localhost:3300/api/quran_rewayah/surah')
-      .then(r => r.json())
-      .then(j => console.log(j))
+  const [Sable, setSable] = useState(true)
+  const [ErrorAd, setErrorAd] = useState(false)
+  const [Error, setError] = useState(null)
+  const [Ayah, setAyah] = useState('')
+  const [Surah, setSurah] = useState('')
+
+  const [Bazzi, setBazzi] = useState([])
+  const [Hisham, setHisham] = useState([])
+  const [Hafs, setHafs] = useState([])
+  const [Susi, setSusi] = useState([])
+  const [Khalaf, setKhalaf] = useState([])
+  const [Warsh, setWarsh] = useState([])
+  const [Laith, setLaith] = useState([])
+  const [Qunbul, setQunbul] = useState([])
+  const [IbnDzakwan, setIbnDzakwan] = useState([])
+  const [Shubah, setShubah] = useState([])
+  const [DuriAbuAmr, setDuriAbuAmr] = useState([])
+  const [Khallad, setKhallad] = useState([])
+  const [Qolun, setQolun] = useState([])
+  const [DuriKisai, setDuriKisai] = useState([])
+
+  const Commit = () => {
+
+    if (document.querySelector('.Qiraah-Bazzi')) {
+      let Qiraah = []
+      document.querySelectorAll('.Qiraah-Bazzi')
+        .forEach(data => {
+          Qiraah.push(data.value)
+        })
+      setBazzi(Qiraah)
+    } else {
+      setBazzi([])
+    }
+
+    if (document.querySelector('.Qiraah-Hisham')) {
+      let Qiraah = []
+      document.querySelectorAll('.Qiraah-Hisham')
+        .forEach(data => {
+          Qiraah.push(data.value)
+        })
+      setHisham(Qiraah)
+    } else {
+      setHisham([])
+    }
+
+    if (document.querySelector('.Qiraah-Hafs')) {
+      let Qiraah = []
+      document.querySelectorAll('.Qiraah-Hafs')
+        .forEach(data => {
+          Qiraah.push(data.value)
+        })
+      setHafs(Qiraah)
+    } else {
+      setHafs([])
+    }
+
+    if (document.querySelector('.Qiraah-Susi')) {
+      let Qiraah = []
+      document.querySelectorAll('.Qiraah-Susi')
+        .forEach(data => {
+          Qiraah.push(data.value)
+        })
+      setSusi(Qiraah)
+    } else {
+      setSusi([])
+    }
+
+    if (document.querySelector('.Qiraah-Khalaf')) {
+      let Qiraah = []
+      document.querySelectorAll('.Qiraah-Khalaf')
+        .forEach(data => {
+          Qiraah.push(data.value)
+        })
+      setKhalaf(Qiraah)
+    } else {
+      setKhalaf([])
+    }
+
+    if (document.querySelector('.Qiraah-Warsh')) {
+      let Qiraah = []
+      document.querySelectorAll('.Qiraah-Warsh')
+        .forEach(data => {
+          Qiraah.push(data.value)
+        })
+      setWarsh(Qiraah)
+    } else {
+      setWarsh([])
+    }
+
+    if (document.querySelector('.Qiraah-Laith')) {
+      let Qiraah = []
+      document.querySelectorAll('.Qiraah-Laith')
+        .forEach(data => {
+          Qiraah.push(data.value)
+        })
+      setLaith(Qiraah)
+    } else {
+      setLaith([])
+    }
+
+    if (document.querySelector('.Qiraah-Qunbul')) {
+      let Qiraah = []
+      document.querySelectorAll('.Qiraah-Qunbul')
+        .forEach(data => {
+          Qiraah.push(data.value)
+        })
+      setQunbul(Qiraah)
+    } else {
+      setQunbul([])
+    }
+
+    if (document.querySelector('.Qiraah-IbnDzakwan')) {
+      let Qiraah = []
+      document.querySelectorAll('.Qiraah-IbnDzakwan')
+        .forEach(data => {
+          Qiraah.push(data.value)
+        })
+      setIbnDzakwan(Qiraah)
+    } else {
+      setIbnDzakwan([])
+    }
+
+    if (document.querySelector('.Qiraah-Shubah')) {
+      let Qiraah = []
+      document.querySelectorAll('.Qiraah-Shubah')
+        .forEach(data => {
+          Qiraah.push(data.value)
+        })
+      setShubah(Qiraah)
+    } else {
+      setShubah([])
+    }
+
+    if (document.querySelector('.Qiraah-DuriAbuAmr')) {
+      let Qiraah = []
+      document.querySelectorAll('.Qiraah-DuriAbuAmr')
+        .forEach(data => {
+          Qiraah.push(data.value)
+        })
+      setDuriAbuAmr(Qiraah)
+    } else {
+      setDuriAbuAmr([])
+    }
+
+    if (document.querySelector('.Qiraah-Khallad')) {
+      let Qiraah = []
+      document.querySelectorAll('.Qiraah-Khallad')
+        .forEach(data => {
+          Qiraah.push(data.value)
+        })
+      setKhallad(Qiraah)
+    } else {
+      setKhallad([])
+    }
+
+    if (document.querySelector('.Qiraah-Qolun')) {
+      let Qiraah = []
+      document.querySelectorAll('.Qiraah-Qolun')
+        .forEach(data => {
+          Qiraah.push(data.value)
+        })
+      setQolun(Qiraah)
+    } else {
+      setQolun([])
+    }
+
+    if (document.querySelector('.Qiraah-DuriKisai')) {
+      let Qiraah = []
+      document.querySelectorAll('.Qiraah-DuriKisai')
+        .forEach(data => {
+          Qiraah.push(data.value)
+        })
+      setDuriKisai(Qiraah)
+    } else {
+      setDuriKisai([])
+    }
+
+    setSable(false)
+    setError('Data Commited, Now Push!')
+    setErrorAd(true)
   }
 
-  useEffect(() => {
-    get()
-  }, [])
+  const Push = async () => {
+
+    setSable(true)
+    await addAyah()
+  }
+
+  const addAyah = async () => {
+
+    const rewayah = {
+      rewayah:
+        [
+          {
+            qori: "عبد الله بن كثير الداري المكي",
+            rowi: [
+              {
+                name: "قنبل",
+                qiraah: Qunbul
+              },
+              {
+                name: "البزي",
+                qiraah: Bazzi
+              }
+            ]
+          },
+          {
+            qori: "عبد الله بن عامر اليحصبي الشامي",
+            rowi: [
+              {
+                name: "هشام",
+                qiraah: Hisham
+              },
+              {
+                name: "ابن ذكوان",
+                qiraah: IbnDzakwan
+              }
+            ]
+          },
+          {
+            qori: "عاصم بن أبي النَّجود الأسدي الكوفي",
+            rowi: [
+              {
+                name: "شعبة",
+                qiraah: Shubah
+              },
+              {
+                name: "حفص",
+                qiraah: Hafs
+              }
+            ]
+          },
+          {
+            qori: "أبو عمرو بن العلاء البصري",
+            rowi: [
+              {
+                name: "السوسي",
+                qiraah: Susi
+              },
+              {
+                name: "الدوري",
+                qiraah: DuriAbuAmr
+              }
+            ]
+          },
+          {
+            qori: "حمزة بن حبيب الزيات الكوفي",
+            rowi: [
+              {
+                name: "خلف",
+                qiraah: Khalaf
+              },
+              {
+                name: "خلاد",
+                qiraah: Khallad
+              }
+            ]
+          },
+          {
+            qori: "نافع بن عبد الرحمن بن أبي نعيم المدني",
+            rowi: [
+              {
+                name: "ورش",
+                qiraah: Warsh
+              },
+              {
+                name: "قالون",
+                qiraah: Qolun
+              }
+            ]
+          },
+          {
+            qori: "أبو الحسن علي بن حمزة الكسائي النحوي الكوفي",
+            rowi: [
+              {
+                name: "ليث",
+                qiraah: Laith
+              },
+              {
+                name: "الدوري",
+                qiraah: DuriKisai
+              }
+            ]
+          }
+        ]
+    }
+
+    await fetch(`http://localhost:3300/api/quran_rewayah/ayah/${Ayah}?surah=${Surah}`, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify(rewayah)
+    })
+      .then(r => r.json())
+      .then(j => {
+        console.log(j)
+        setError(j.message)
+        setErrorAd(true)
+      })
+  }
 
   const addInput = (a) => {
+
     const input = document.createElement('input')
     input.type = 'text'
     input.placeholder = `Qiraah of ${a}...`
-    input.className = `mt-2 bg-transparent py-2 px-3 rounded-lg text-lg  border outline-none border-slate-400 Qiraah-${a}`
+    input.className = `mt-2 bg-transparent text-white py-2 px-3 rounded-lg text-lg  border outline-none border-slate-400 Qiraah-${a}`
 
     document.querySelector(`.${a}`).append(input)
   }
 
   return (
-    <div className="flex flex-col mx-auto mt-10 mb-16 w-[700px]">
+    <div className="flex flex-col mx-auto mb-16 w-[700px]">
+      {
+        (ErrorAd == true) && <div className="text-xl bg-slate-500 py-2 px-3 font-bold mb-4 rounded-lg text-white flex justify-between items-center">
+          <div>{Error}</div>
+          <button onClick={() => setErrorAd(false)} className="text-slate-600 hover:text-red-700 text-2xl font-bold">&times;</button>
+        </div>
+      }
       <div className="flex justify-between items-center">
         <div className="flex">
-          <input type="number" max="286" min="1" placeholder="Number of Ayah" className="mr-2 bg-transparent py-2 px-3 rounded-lg text-lg  border outline-none border-slate-400" />
-          <input type="number" max="144" min="1" placeholder="Number of Surah" className="ml-2 bg-transparent py-2 px-3 rounded-lg text-lg  border outline-none border-slate-400" />
+          <input type="number" max="286" min="1" autoFocus placeholder="Number of Ayah" className="mr-2 text-white w-56 bg-transparent py-2 px-3 rounded-lg text-lg  border outline-none border-slate-400" onChange={(e) => setAyah(e.target.value)} />
+          <input type="number" max="144" min="1" placeholder="Number of Surah" className="ml-2 text-white w-56 bg-transparent py-2 px-3 rounded-lg text-lg  border outline-none border-slate-400" onChange={(e) => setSurah(e.target.value)} />
         </div>
-        <button className=" bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-5 rounded-lg text-lg" >Save Ayah</button>
+        <div className="flex">
+          <button onClick={() => Commit()} className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded-lg text-lg mr-2" >Commit</button>
+          {
+            (Sable == true) ? <button disabled onClick={() => Push()} className="bg-green-700 cursor-not-allowed font-bold py-2 px-5 rounded-lg text-lg" >Push</button> : <button onClick={() => Push()} className=" bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-5 rounded-lg text-lg" >Push</button>
+          }
+
+        </div>
       </div>
       <hr className="my-5" />
       <div className="flex justify-between">
