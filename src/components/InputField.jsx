3bb216805a0e3d00a933a9a08344e1420137006d
saifuddien <ariefsaifuddien01@gmail.com>
2022-06-04 17:46:56 +0200

    update delete feature

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import quraa from './quraa.json'
 
 function InputField() {
 
   const [Sable, setSable] = useState(true)
-  const [ErrorAd, setErrorAd] = useState(false)
+  const [Commited, setCommited] = useState(false)
+  const [Errored, setErrored] = useState(false)
   const [Error, setError] = useState(null)
-  const [Ayah, setAyah] = useState('')
-  const [Surah, setSurah] = useState('')
+  const [Ayah, setAyah] = useState(null)
+  const [Surah, setSurah] = useState(null)
 
   const [Bazzi, setBazzi] = useState([])
   const [Hisham, setHisham] = useState([])
@@ -23,6 +24,14 @@ function InputField() {
   const [Khallad, setKhallad] = useState([])
   const [Qolun, setQolun] = useState([])
   const [DuriKisai, setDuriKisai] = useState([])
+
+  useEffect(() => {
+    if (document.querySelector('.ayah').value != '' && document.querySelector('.surah').value != '') {
+      setCommited(true)
+    } else {
+      setCommited(false)
+    }
+  }, [Ayah, Surah])
 
   const Commit = () => {
 
@@ -181,114 +190,143 @@ function InputField() {
     }
 
     setSable(false)
-    setError('Data Commited, Now Push!')
-    setErrorAd(true)
+    setError('Data Commited!')
+    setErrored(true)
   }
 
-  const Push = async () => {
+  const rewayah = {
+    rewayah:
+      [
+        {
+          qori: "عبد الله بن كثير الداري المكي",
+          rowi: [
+            {
+              name: "قنبل",
+              qiraah: Qunbul
+            },
+            {
+              name: "البزي",
+              qiraah: Bazzi
+            }
+          ]
+        },
+        {
+          qori: "عبد الله بن عامر اليحصبي الشامي",
+          rowi: [
+            {
+              name: "هشام",
+              qiraah: Hisham
+            },
+            {
+              name: "ابن ذكوان",
+              qiraah: IbnDzakwan
+            }
+          ]
+        },
+        {
+          qori: "عاصم بن أبي النَّجود الأسدي الكوفي",
+          rowi: [
+            {
+              name: "شعبة",
+              qiraah: Shubah
+            },
+            {
+              name: "حفص",
+              qiraah: Hafs
+            }
+          ]
+        },
+        {
+          qori: "أبو عمرو بن العلاء البصري",
+          rowi: [
+            {
+              name: "السوسي",
+              qiraah: Susi
+            },
+            {
+              name: "الدوري",
+              qiraah: DuriAbuAmr
+            }
+          ]
+        },
+        {
+          qori: "حمزة بن حبيب الزيات الكوفي",
+          rowi: [
+            {
+              name: "خلف",
+              qiraah: Khalaf
+            },
+            {
+              name: "خلاد",
+              qiraah: Khallad
+            }
+          ]
+        },
+        {
+          qori: "نافع بن عبد الرحمن بن أبي نعيم المدني",
+          rowi: [
+            {
+              name: "ورش",
+              qiraah: Warsh
+            },
+            {
+              name: "قالون",
+              qiraah: Qolun
+            }
+          ]
+        },
+        {
+          qori: "أبو الحسن علي بن حمزة الكسائي النحوي الكوفي",
+          rowi: [
+            {
+              name: "ليث",
+              qiraah: Laith
+            },
+            {
+              name: "الدوري",
+              qiraah: DuriKisai
+            }
+          ]
+        }
+      ]
+  }
+
+  const updateAyah = async () => {
 
     setSable(true)
-    await addAyah()
+
+    await fetch(`http://localhost:3300/api/quran_rewayah/ayah/${Ayah}?surah=${Surah}`, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: 'PUT',
+      body: JSON.stringify(rewayah)
+    })
+      .then(r => r.json())
+      .then(j => {
+        setError(j.message)
+        setErrored(true)
+      })
+  }
+
+  const deleteAyah = async () => {
+
+    setSable(true)
+
+    await fetch(`http://localhost:3300/api/quran_rewayah/ayah/${Ayah}?surah=${Surah}`, {
+      method: 'DELETE',
+    })
+      .then(r => r.json())
+      .then(j => {
+        setError(j.message)
+        setErrored(true)
+      })
   }
 
   const addAyah = async () => {
 
-    const rewayah = {
-      rewayah:
-        [
-          {
-            qori: "عبد الله بن كثير الداري المكي",
-            rowi: [
-              {
-                name: "قنبل",
-                qiraah: Qunbul
-              },
-              {
-                name: "البزي",
-                qiraah: Bazzi
-              }
-            ]
-          },
-          {
-            qori: "عبد الله بن عامر اليحصبي الشامي",
-            rowi: [
-              {
-                name: "هشام",
-                qiraah: Hisham
-              },
-              {
-                name: "ابن ذكوان",
-                qiraah: IbnDzakwan
-              }
-            ]
-          },
-          {
-            qori: "عاصم بن أبي النَّجود الأسدي الكوفي",
-            rowi: [
-              {
-                name: "شعبة",
-                qiraah: Shubah
-              },
-              {
-                name: "حفص",
-                qiraah: Hafs
-              }
-            ]
-          },
-          {
-            qori: "أبو عمرو بن العلاء البصري",
-            rowi: [
-              {
-                name: "السوسي",
-                qiraah: Susi
-              },
-              {
-                name: "الدوري",
-                qiraah: DuriAbuAmr
-              }
-            ]
-          },
-          {
-            qori: "حمزة بن حبيب الزيات الكوفي",
-            rowi: [
-              {
-                name: "خلف",
-                qiraah: Khalaf
-              },
-              {
-                name: "خلاد",
-                qiraah: Khallad
-              }
-            ]
-          },
-          {
-            qori: "نافع بن عبد الرحمن بن أبي نعيم المدني",
-            rowi: [
-              {
-                name: "ورش",
-                qiraah: Warsh
-              },
-              {
-                name: "قالون",
-                qiraah: Qolun
-              }
-            ]
-          },
-          {
-            qori: "أبو الحسن علي بن حمزة الكسائي النحوي الكوفي",
-            rowi: [
-              {
-                name: "ليث",
-                qiraah: Laith
-              },
-              {
-                name: "الدوري",
-                qiraah: DuriKisai
-              }
-            ]
-          }
-        ]
-    }
+    setSable(true)
 
     await fetch(`http://localhost:3300/api/quran_rewayah/ayah/${Ayah}?surah=${Surah}`, {
       headers: {
@@ -300,9 +338,8 @@ function InputField() {
     })
       .then(r => r.json())
       .then(j => {
-        console.log(j)
         setError(j.message)
-        setErrorAd(true)
+        setErrored(true)
       })
   }
 
@@ -319,22 +356,29 @@ function InputField() {
   return (
     <div className="flex flex-col mx-auto mb-16 w-[700px]">
       {
-        (ErrorAd == true) && <div className="text-xl bg-slate-500 py-2 px-3 font-bold mb-4 rounded-lg text-white flex justify-between items-center">
+        (Errored == true) && <div className="text-xl bg-slate-500 py-2 px-3 font-bold mb-4 rounded-lg text-white flex justify-between items-center">
           <div>{Error}</div>
-          <button onClick={() => setErrorAd(false)} className="text-slate-600 hover:text-red-700 text-2xl font-bold">&times;</button>
+          <button onClick={() => setErrored(false)} className="text-slate-600 hover:text-red-700 text-2xl font-bold">&times;</button>
         </div>
       }
       <div className="flex justify-between items-center">
         <div className="flex">
-          <input type="number" max="286" min="1" autoFocus placeholder="Number of Ayah" className="mr-2 text-white w-56 bg-transparent py-2 px-3 rounded-lg text-lg  border outline-none border-slate-400" onChange={(e) => setAyah(e.target.value)} />
-          <input type="number" max="144" min="1" placeholder="Number of Surah" className="ml-2 text-white w-56 bg-transparent py-2 px-3 rounded-lg text-lg  border outline-none border-slate-400" onChange={(e) => setSurah(e.target.value)} />
+          <input type="number" max="286" min="1" autoFocus placeholder="No of Ayah" className="mr-2 ayah text-white w-40 bg-transparent py-2 px-3 rounded-lg text-lg  border outline-none border-slate-400" onChange={(e) => setAyah(e.target.value)} />
+          <input type="number" max="144" min="1" placeholder="No of Surah" className="surah text-white w-40 bg-transparent py-2 px-3 rounded-lg text-lg  border outline-none border-slate-400" onChange={(e) => setSurah(e.target.value)} />
         </div>
         <div className="flex">
-          <button onClick={() => Commit()} className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded-lg text-lg mr-2" >Commit</button>
           {
-            (Sable == true) ? <button disabled onClick={() => Push()} className="bg-green-700 cursor-not-allowed font-bold py-2 px-5 rounded-lg text-lg" >Push</button> : <button onClick={() => Push()} className=" bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-5 rounded-lg text-lg" >Push</button>
+            (Commited == false) ? <button onClick={() => Commit()} disabled className=" bg-blue-700 font-bold py-2 px-5 rounded-lg text-lg mr-2" >Commit</button> : <button onClick={() => Commit()} className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded-lg text-lg mr-2" >Commit</button>
           }
-
+          {
+            (Sable == true) ? <button disabled onClick={() => addAyah()} className="bg-green-700 cursor-not-allowed font-bold py-2 px-5 rounded-lg text-lg" >Add</button> : <button onClick={() => addAyah()} className=" bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-5 rounded-lg text-lg" >Add</button>
+          }
+          {
+            (Sable == true) ? <button disabled onClick={() => updateAyah()} className="bg-green-700 cursor-not-allowed font-bold py-2 px-5 rounded-lg text-lg ml-2" >Update</button> : <button onClick={() => updateAyah()} className=" bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-5 rounded-lg text-lg ml-2" >Update</button>
+          }
+          {
+            (Commited == false) ? <button onClick={() => deleteAyah()} disabled className=" bg-red-700 font-bold py-2 px-3 rounded-lg text-lg ml-2" ><i className="fa fa-trash-can"></i></button> : <button onClick={() => deleteAyah()} className=" bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-3 rounded-lg text-lg ml-2" ><i className="fa fa-trash-can"></i></button>
+          }
         </div>
       </div>
       <hr className="my-5" />
