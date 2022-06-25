805992ff8ae172c9e874fe3997de38b58bc39a12
saifuddien <ariefsaifuddien01@gmail.com>
2022-06-25 23:17:33 +0200

    pcss

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
+import './index.pcss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
